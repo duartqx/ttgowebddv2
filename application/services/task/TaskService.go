04f53ce60ff6c29baf5bafcc8d42e94809860722
Diego Duarte <duartqx@gmail.com>
2024-03-24 20:20:44 -0300

    TaskService

---
+++ application/services/task/TaskService.go
@@ -0,0 +1,62 @@
+package task
+
+import (
+	"fmt"
+
+	e "github.com/duartqx/ttgowebddv2/common/errors"
+	t "github.com/duartqx/ttgowebddv2/domains/task"
+)
+
+type TaskService struct {
+	taskRepository t.ITaskRepository
+}
+
+var taskService *TaskService
+
+func GetTaskService(taskRepository t.ITaskRepository) *TaskService {
+	if taskService == nil {
+		taskService = &TaskService{taskRepository: taskRepository}
+	}
+	return taskService
+}
+
+func (ts TaskService) Filter(tf t.ITaskFilter) (*[]t.Task, error) {
+	tasks, err := ts.taskRepository.Filter(tf)
+	if err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
+
+func (ts TaskService) isInvalidTask(task *t.Task) bool {
+	return task.GetTag() == "" || task.GetSprint() == ""
+
+}
+
+func (ts TaskService) Create(task *t.Task) error {
+	if ts.isInvalidTask(task) {
+		return fmt.Errorf("%w: Invalid Task", e.BadRequestError)
+	}
+
+	if err := ts.taskRepository.Create(task); err != nil {
+		return fmt.Errorf("%w: Error Trying to Create Task", e.InternalError)
+	}
+
+	return nil
+}
+
+func (ts TaskService) Update(task *t.Task) error {
+	if task.GetId() == 0 || ts.isInvalidTask(task) {
+		return fmt.Errorf("%w: Invalid Task", e.BadRequestError)
+	}
+
+	if err := ts.taskRepository.Update(task); err != nil {
+		return fmt.Errorf("%w: Error Trying to Update Task", e.InternalError)
+	}
+
+	return nil
+}
+
+func (ts TaskService) GetSprints() *[]string {
+	return ts.taskRepository.GetSprints()
+}
