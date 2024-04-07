cee85540d37f9194f38ff9e556abd64fef8454ec
Diego Duarte <duartqx@gmail.com>
2024-04-06 23:28:48 -0300

    pwa

--- internal/presentation/react/vite.config.js
+++ internal/presentation/react/vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// Manifest configuration
+const manifest = {
+    name: "Tasks",
+    short_name: "Tasks",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#18181b",
+    theme_color: "#18181b",
+    description: "Tasks - ttgowebddv2",
+    icons: [
+        {
+            src: "icon/lowres.webp",
+            sizes: "48x48",
+            type: "image/webp",
+        },
+    ],
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), VitePWA({ manifest })],
 });
